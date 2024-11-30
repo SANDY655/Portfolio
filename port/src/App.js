499bb7af643ba10e20e31ffa776110726c6889b2
SANDY655 <149648197+SANDY655@users.noreply.github.com>
2024-11-30 20:14:01 +0530

    Update App.js

--- port/src/App.js
+++ port/src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
-import { Analytics } from "@vercel/analytics/react"
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
