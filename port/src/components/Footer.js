3a1216ff0b7f95b0e93c310f14966fd579273af3
SANDY655 <149648197+SANDY655@users.noreply.github.com>
2025-01-31 18:47:22 +0530

    Update Footer.js

--- port/src/components/Footer.js
+++ port/src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2024 Your Name. All rights reserved.</p>
+      <p>© 2025 Santhosh. All rights reserved.</p>
     </footer>
   );
 };
